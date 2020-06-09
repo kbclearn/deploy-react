87bd8b30a75e0533cf21e332cdafaad6f5076a57
satya prakash singh <kbclearn@gmail.com>
2020-06-09 16:35:14 +0530

    Updates

---
+++ precache-manifest.d82bd6e8a1c9847accc5cb1d8829e4c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d201d43d98ab71615b130de1de8139fe",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "a1f18883126d51f36b56",
+    "url": "/deploy-react/static/css/main.131f0dd0.chunk.css"
+  },
+  {
+    "revision": "a81346df5bd98647e085",
+    "url": "/deploy-react/static/js/2.f5a5d6ab.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/deploy-react/static/js/2.f5a5d6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f18883126d51f36b56",
+    "url": "/deploy-react/static/js/main.cf2d7e60.chunk.js"
+  },
+  {
+    "revision": "2820ea3bdc9a66ee6a53",
+    "url": "/deploy-react/static/js/runtime-main.3d487ca6.js"
+  }
+]);
